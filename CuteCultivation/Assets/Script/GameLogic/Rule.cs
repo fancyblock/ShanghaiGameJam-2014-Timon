5437daf1dd95f5c5f88fe3a9845630873484d098
fancyblock <xujia1985@gmail.com>
2014-10-18 02:18:29 +0800

    commit before goto bed

---
+++ CuteCultivation/Assets/Script/GameLogic/Rule.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rule 
+{
+    /// <summary>
+    /// constructor 
+    /// </summary>
+    public Rule()
+    {
+        //TODO 
+    }
+
+    public eMoumouType DEST_MOUMOU { get; set; }
+
+}
